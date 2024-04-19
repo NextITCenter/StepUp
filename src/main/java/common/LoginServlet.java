3ee33efd17128d9f4ce4a1b14f369f5c9cdea6a7
nextit <nextitcenter@gmail.com>
2024-04-19 09:35:26 +0900

    로그인 mybatis 적용

--- src/main/java/common/LoginServlet.java
+++ src/main/java/common/LoginServlet.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -16,12 +17,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
+
+import member.MemberService;
 import member.MemberVO;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private MemberService service;
+	
+	@Override
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		SqlSession session = (SqlSession) servletContext.getAttribute("sqlSession");
+		this.service = new MemberService(session);
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/common/login.jsp").forward(request, response);
 	}
@@ -40,52 +51,21 @@ public class LoginServlet extends HttpServlet {
 		}
 		response.addCookie(cookie);
 		
+		MemberVO vo = service.currentPassword(new MemberVO(searchId, searchPassword));
+		
 		// id에 해당하는 데이터를 조회하여 가져온다.
-		Connection connection = null;
-		PreparedStatement statement = null;
-		ResultSet resultSet = null;
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@nextit.or.kr:1521:xe", "java", "oracle21c");
-			String sql = "select id, name, email, create_date from member where id = ? and password = ?";
-			statement = connection.prepareStatement(sql);
-			statement.setString(1, searchId);
-			statement.setString(2, searchPassword);
+		if (vo != null) {
+			// 서버에 사용자 정보를 저장하고 /member/list로 이동
+			// 세션은 HttpSession 객체를 통해서 관리할 수 있다.
+			HttpSession session = request.getSession();
+			session.setAttribute("member", vo);
 			
-			resultSet = statement.executeQuery();
-			MemberVO vo = null;
-			if (resultSet.next()) {
-				String id = resultSet.getString("id");
-				String name = resultSet.getString("name");
-				String email = resultSet.getString("email");
-				Date createDate = resultSet.getDate("create_date");
-				vo = new MemberVO(id, name, email, createDate.toLocalDate());
-			}
-			if (vo != null) {
-				// 서버에 사용자 정보를 저장하고 /member/list로 이동
-				// 세션은 HttpSession 객체를 통해서 관리할 수 있다.
-				HttpSession session = request.getSession();
-				session.setAttribute("member", vo);
-				
-				response.sendRedirect("/member/list");
-			} else {
-				// 사용자가 없으면 로그인 페이지에 해당하는 사용자가 없다고 알려줌
-				request.setAttribute("msg", "해당하는 사용자가 존재하지 않습니다.");
-				request.getRequestDispatcher("/WEB-INF/views/common/login.jsp").forward(request, response);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				connection.close();
-				statement.close();
-				resultSet.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			response.sendRedirect("/member/list");
+		} else {
+			// 사용자가 없으면 로그인 페이지에 해당하는 사용자가 없다고 알려줌
+			request.setAttribute("msg", "해당하는 사용자가 존재하지 않습니다.");
+			request.getRequestDispatcher("/WEB-INF/views/common/login.jsp").forward(request, response);
 		}
-
 		
 	}
 }
