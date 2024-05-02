e3ef328afc2c0bc37b21a91d39b5e1d1e3f1bdfa
gks930620 <gks930620@naver.com>
2024-05-02 13:51:11 +0900

    게시판 파일 등록

--- src/main/java/board/InsertServlet.java
+++ src/main/java/board/InsertServlet.java
@@ -1,19 +1,23 @@
 package board;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.ibatis.session.SqlSession;
 
 /**
  * controller 역할
  */
+@MultipartConfig(location = "c:\\temp")
 @WebServlet("/board/add")
 public class InsertServlet extends HttpServlet {
 	private BoardService service;
@@ -33,6 +37,40 @@ public class InsertServlet extends HttpServlet {
 		String writer = request.getParameter("writer");
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
+		// 모든 input 태그들은 multipart/form-data로 전송시 part에 담긴다.
+		// 이때 파일을 제외한 나머지는 request.getParameter()로 가져오는게 편하고
+		// 첨부파일만 getPart()를 통해 가져온다.
+		Part part = request.getPart("uploadFile");
+		// 업로드된 첨부파일의 파일 크기를 알고 싶으면
+		long fileSize = part.getSize();
+		// 업로드된 첨부파일의 이름을 알고 싶으면
+		String fileName = part.getSubmittedFileName();
+		System.out.println("file size: " + fileSize);
+		System.out.println("file name: " + fileName);
+		// 파일 쓰기
+		part.write("c:\\temp\\" + fileName);
+		part.delete();
+		
+//		String contentType = request.getContentType();
+//		if (contentType != null && contentType.toLowerCase().startsWith("multipart/")) {
+//			Collection<Part> parts = request.getParts();
+//			for (Part part : parts) {
+//				System.out.println("name: " + part.getName());
+//				String type = part.getContentType();
+//				if (part.getHeader("Content-Disposition").contains("filename=")) {
+//					System.out.println("file size: " + part.getSize());
+//					String fileName = part.getSubmittedFileName();
+//					System.out.println("file name: " + fileName);
+//					if (part.getSize() > 0) {
+//						part.write("c:\\upload_temp\\" + fileName);
+//						part.delete();
+//					}
+//				} else {
+//					String value = request.getParameter(part.getName());
+//					System.out.println("value: " + value);
+//				}
+//			}
+//		}
 		
 		int insertBoard = service.insertBoard(new BoardVO(writer, title, content));
 		if (insertBoard > 0) {
--- src/main/webapp/WEB-INF/views/board/add.jsp
+++ src/main/webapp/WEB-INF/views/board/add.jsp
@@ -3,12 +3,15 @@
 <jsp:include page="/WEB-INF/views/common/header.jsp">
 	<jsp:param value="게시글 등록" name="title"/>
 </jsp:include>
-<form action="/board/add" method="post">
+<form action="/board/add" method="post" enctype="multipart/form-data">
 	<label>제목:
 		<input type="text" name="title">
 	</label>
 	<label>내용:
 		<textarea rows="10" cols="40" name="content"></textarea>
+	</label>
+	<label>
+		<input type="file" name="uploadFile">
 	</label>
 	<input type="hidden" name="writer" value="${member.id }">
 	<div style="color:red;">${msg}</div>
